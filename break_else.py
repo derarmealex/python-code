4edbf9bceeec2c91a3e30963b4b3a33e027515ba
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-07-30 13:26:06 +0200

    Add files via upload

---
+++ break_else.py
@@ -0,0 +1,29 @@
+for letter in ("a", "b", "c", "d"):
+    print(letter.upper())
+else:
+    print("All the letters are written down")
+    
+print("\n")
+
+for letter in ("a", "b", "c", "d"):
+    if letter == "4":
+        print("Found '4', skip the cycle")
+        break
+else:
+    # kvůli 'break' se tento text nevypíše po ukončení smyčky
+    print("All the letters are written down")
+
+print("Go on the loop")
+print("\n")
+
+for letter in ("a", "b", "c", "d"):
+    if letter == "c":
+        print("Found 'c', skip the cycle")
+        break
+else:
+    # kvůli 'break' se tento text nevypíše po ukončení smyčky
+    print("All the letters are written down")
+    
+print("Go on the loop")
+
+input()

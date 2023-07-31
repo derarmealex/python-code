a8191c3a8d80adc9084f9d44b8d849b2668df3a4
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-07-31 14:34:23 +0200

    Delete immersed_loop.py

--- immersed_loop.py
+++
@@ -1,9 +0,0 @@
-lpdlst = [
-  ["Matous", "Marek", "Lukas", "Jan"],
-  ["Lucie", "Aneta", "Eva", "Lenka"],
-  ["Helmut", "Hammet", "Hetfield", "Harold"]
-]
-for lst in lpdlst:
-    for name in lst:
-        for letter in name:
-            print(letter)

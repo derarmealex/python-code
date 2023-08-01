8ab0745f2f2f8369b9811d02d22dfa9b79d1cd83
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-02 00:13:52 +0200

    Update converter us-eu.py

--- converter us-eu.py
+++ converter us-eu.py
@@ -1,8 +1,30 @@
-# converse lb to kg
-print("Enter libres:")
-lb = float(input())
-print(lb, "lb is", round(lb/2.2, 2), "kg", "\n")
-
+# converse Lb to kg
+repeat = True
+while repeat:
+    Lb = input("Enter libres:\n")
+    if Lb.isnumeric():
+        print(Lb, "Lb is", round(int(Lb)/2.2, 2), "kg", "\n")
+        repeat = False
+        another_Lb = input("Do you have another value for measure? y/n\n")
+        if another_Lb == "y":
+            repeat = True
+        elif another_Lb == "n":
+            pass
+        else:
+            pass
+    elif "." in Lb:
+        print(Lb, "Lb is", round(float(Lb)/2.2, 2), "kg", "\n")
+        repeat = False
+        another_Lb = input("Do you have another value for measure? y/n\n")
+        if another_Lb == "y":
+            repeat = True
+        elif another_Lb == "n":
+            pass
+        else:
+            pass
+    else:
+        print("Wrong value, enter a number")
+    
 # converse mile to km
 print("Enter miles:")
 mile = float(input())
@@ -12,3 +34,5 @@ print(mile, "mile is", round(mile/0.62, 2), "km", "\n")
 print("Enter gallons:")
 gal = float(input())
 print(gal, "gal is", round(gal/0.26, 2), "l", "\n")
+
+input()

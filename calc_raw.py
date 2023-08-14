1b11afc8ee3abacadf8ab27829cc5304247d469d
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-14 23:01:44 +0200

    Add files via upload

---
+++ calc_raw.py
@@ -0,0 +1,25 @@
+print('Summation:    "+",\nSubtraction:   "-",\n----------------------')
+while True:
+    oper = input("Choose the operation: ")
+    if oper not in ('+','-'):
+        print("Wrong operator. Try again.")
+        continue
+    else:
+        ctr = True
+        while ctr:
+            num1 = input("Enter your first number: ")
+            num2 = input("Enter your second number: ")
+            if not num1.isnumeric() or not num2.isnumeric():
+                print("Enter a number!")
+                continue
+            elif oper == "+":
+                print(f"{int(num1)} + {int(num2)} = {int(num1) + int(num2)}")
+            else:
+                print(f"{int(num1)} - {int(num2)} = {int(num1) - int(num2)}")
+            while True:
+                goon = input("Like to do another operation?('y' for Yes, any other key for No): ")
+                if goon in ('y', 'Y'):
+                    ctr = False
+                    break
+                else:
+                    exit()

a42ae11dc9befc2c318cc8a38a7cf2334d68bf82
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-14 15:16:31 +0200

    Update converter_lb_kg.py

--- converter_lb_kg.py
+++ converter_lb_kg.py
@@ -3,7 +3,7 @@ while True:
     if lb.isnumeric():
         print(lb, "lb is", round(int(lb)/2.2, 2), "kg", "\n")
         while True:
-            another_choise = input("Do you have another value for measure? y/n\n")
+            another_choise = input("Do you have another value to measure? y/n\n")
             if another_choise == "y":
                 break
             elif another_choise == "n":
@@ -15,7 +15,7 @@ while True:
     elif "." in lb:
         print(lb, "lb is", round(float(lb)/2.2, 2), "kg", "\n")
         while True:
-            another_choise = input("Do you have another value for measure? y/n\n")
+            another_choise = input("Do you have another value to measure? y/n\n")
             if another_choise == "y":
                 break
             elif another_choise == "n":

d560b0b696d8b28abe9683b340863af27ff7aa8c
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-14 14:53:27 +0200

    Update converter_gal_l.py

--- converter_gal_l.py
+++ converter_gal_l.py
@@ -3,7 +3,7 @@ while True:
     if gal.isnumeric():
         print(gal, "gal is", round(int(gal)/0.26, 2), "l", "\n")
         while True:
-            another_choise = input("Do you have another value for measure? y/n\n")
+            another_choise = input("Do you have another value to measure? y/n\n")
             if another_choise == "y":
                 break
             elif another_choise == "n":
@@ -15,7 +15,7 @@ while True:
     elif "." in gal:
         print(gal, "gal is", round(float(gal)/0.26, 2), "l", "\n")
         while True:
-            another_choise = input("Do you have another value for measure? y/n\n")
+            another_choise = input("Do you have another value to measure? y/n\n")
             if another_choise == "y":
                 break
             elif another_choise == "n":

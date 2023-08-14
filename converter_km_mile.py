ea30c7fe1fc217025b6f4216308e8d7119809950
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-14 14:53:49 +0200

    Update converter_km_mile.py

--- converter_km_mile.py
+++ converter_km_mile.py
@@ -3,7 +3,7 @@ while True:
     if km.isnumeric():
         print(km, "km is", round(int(km)*0.62, 2), "mile", "\n")
         while True:
-            another_choise = input("Do you have another value for measure? y/n\n")
+            another_choise = input("Do you have another value to measure? y/n\n")
             if another_choise == "y":
                 break
             elif another_choise == "n":
@@ -15,7 +15,7 @@ while True:
     elif "." in km:
         print(km, "km is", round(float(km)*0.62, 2), "mile", "\n")
         while True:
-            another_choise = input("Do you have another value for measure? y/n\n")
+            another_choise = input("Do you have another value to measure? y/n\n")
             if another_choise == "y":
                 break
             elif another_choise == "n":

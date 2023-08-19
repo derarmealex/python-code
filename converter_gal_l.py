b0cbb5834dd7a67a74d08deb4599fad606645751
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-19 21:16:30 +0200

    Update converter_gal_l.py

--- converter_gal_l.py
+++ converter_gal_l.py
@@ -1,28 +1,15 @@
+import re
 while True:
     gal = input("Enter gallons:\n")
-    if gal.isnumeric():
-        print(gal, "gal is", round(int(gal)/0.26, 2), "l", "\n")
-        while True:
-            another_choise = input("Do you have another value to measure? y/n\n")
-            if another_choise == "y":
-                break
-            elif another_choise == "n":
-                print("\nSee ya again!")
-                input("\nPress any key")
-                exit()
-            else:
-                print("\nPress 'y' or 'n'".upper())
-    elif "." in gal:
-        print(gal, "gal is", round(float(gal)/0.26, 2), "l", "\n")
-        while True:
-            another_choise = input("Do you have another value to measure? y/n\n")
-            if another_choise == "y":
-                break
-            elif another_choise == "n":
-                print("\nSee ya again!")
-                input("\nPress any key")
-                exit()
-            else:
-                print("\nPress 'y' or 'n'".upper())
-    else:
+    if re.findall('[^.0123456789]', gal) or gal == '.' or gal == '':
         print("Wrong value, enter a number".upper())
+    else:
+        print(float(gal), "gal is", round(float(gal)/0.26, 2), "l", "\n")
+        while True:
+            another_choise = input("Do you have another value to measure? 'y' for 'Yes', any key for 'No'\n").lower()
+            if another_choise == "y":
+                break
+            else:
+                print("\nSee ya again!")
+                input("Press any key to exit")
+                exit()

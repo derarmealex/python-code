c7baefb60adcdc92a43be99e79a19b9fd0c23a83
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-20 15:24:48 +0200

    Update converter_kg_lb.py

--- converter_kg_lb.py
+++ converter_kg_lb.py
@@ -1,28 +1,15 @@
+import re
 while True:
     kg = input("Enter kilograms:\n")
-    if kg.isnumeric():
-        print(kg, "kg is", round(int(kg)*2.2, 2), "lb", "\n")
-        while True:
-            another_choise = input("Do you have another value to measure? y/n\n")
-            if another_choise == "y":
-                break
-            elif another_choise == "n":
-                print("\nSee ya again!")
-                input("\nPress any key")
-                exit()
-            else:
-                print("\nPress 'y' or 'n'".upper())
-    elif "." in kg:
-        print(kg, "kg is", round(float(kg)*2.2, 2), "lb", "\n")
-        while True:
-            another_choise = input("Do you have another value to measure? y/n\n")
-            if another_choise == "y":
-                break
-            elif another_choise == "n":
-                print("\nSee ya again!")
-                input("\nPress any key")
-                exit()
-            else:
-                print("\nPress 'y' or 'n'".upper())
-    else:
+    if re.findall('[^.\d]', kg) or kg == '.' or kg == '':
         print("Wrong value, enter a number".upper())
+    else:
+        print(float(kg), "kg is", round(float(kg)/2.2, 2), "lb", "\n")
+        while True:
+            another_choise = input("Another value to measure? 'y' for 'Yes', any other key for 'No'\n").lower()
+            if another_choise == "y":
+                break
+            else:
+                print("\nSee ya again!")
+                input("Any key to exit")
+                exit()

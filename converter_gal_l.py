f7e635cec265ae70a083f8d363b4f4c1bc4712ae
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-20 15:24:38 +0200

    Update converter_gal_l.py

--- converter_gal_l.py
+++ converter_gal_l.py
@@ -1,15 +1,15 @@
 import re
 while True:
     gal = input("Enter gallons:\n")
-    if re.findall('[^.0123456789]', gal) or gal == '.' or gal == '':
+    if re.findall('[^.\d]', gal) or gal == '.' or gal == '':
         print("Wrong value, enter a number".upper())
     else:
         print(float(gal), "gal is", round(float(gal)/0.26, 2), "l", "\n")
         while True:
-            another_choise = input("Do you have another value to measure? 'y' for 'Yes', any key for 'No'\n").lower()
+            another_choise = input("Another value to measure? 'y' for 'Yes', any other key for 'No'\n").lower()
             if another_choise == "y":
                 break
             else:
                 print("\nSee ya again!")
-                input("Press any key to exit")
+                input("Any key to exit")
                 exit()

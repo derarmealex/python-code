93f9fc5c419f32e6a55af16fee398a1bb8432d60
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-20 15:27:20 +0200

    Update converter_km_mile.py

--- converter_km_mile.py
+++ converter_km_mile.py
@@ -1,28 +1,15 @@
+import re
 while True:
     km = input("Enter kilometres:\n")
-    if km.isnumeric():
-        print(km, "km is", round(int(km)*0.62, 2), "mile", "\n")
-        while True:
-            another_choise = input("Do you have another value to measure? y/n\n")
-            if another_choise == "y":
-                break
-            elif another_choise == "n":
-                print("\nSee ya again!")
-                input("\nPress any key")
-                exit()
-            else:
-                print("\nPress 'y' or 'n'".upper())
-    elif "." in km:
-        print(km, "km is", round(float(km)*0.62, 2), "mile", "\n")
-        while True:
-            another_choise = input("Do you have another value to measure? y/n\n")
-            if another_choise == "y":
-                break
-            elif another_choise == "n":
-                print("\nSee ya again!")
-                input("\nPress any key")
-                exit()
-            else:
-                print("\nPress 'y' or 'n'".upper())
-    else:
+    if re.findall('[^.\d]', km) or km == '.' or km == '':
         print("Wrong value, enter a number".upper())
+    else:
+        print(float(km), "km is", round(float(km) * 0.62, 2), "mile", "\n")
+        while True:
+            another_choise = input("Another value to measure? 'y' for 'Yes', any other key for 'No'\n").lower()
+            if another_choise == "y":
+                break
+            else:
+                print("\nSee ya again!")
+                input("Any key to exit")
+                exit()

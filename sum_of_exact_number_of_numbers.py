e0e8ed7e4eaa17501805077e599c019abe6586f3
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-28 17:13:58 +0200

    Update sum_of_exact_number_of_numbers.py

--- sum_of_exact_number_of_numbers.py
+++ sum_of_exact_number_of_numbers.py
@@ -5,9 +5,9 @@ def summus(x, n):
         summ += x[ix]
         ix += 1
     return summ
-print(summus([1, 2, 3, 4, 5], 3))
+print(summus([1, 2, 3, 4, 5], 3))                                   #6
 # or
 def summus(x, n):
     summ = [0 + number for ix, number in enumerate(x) if ix < n]
     return sum(summ)
-print(summus([1, 2, 3, 4, 5], 3))
+print(summus([1, 2, 3, 4, 5], 3))                                   # 6

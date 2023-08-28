41970dba461ac38d2e3192768af577e56544a1eb
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-28 17:58:09 +0200

    Add files via upload

---
+++ converter_foot_cm.py
@@ -0,0 +1,15 @@
+import re
+while True:
+    ft = input("Enter ft:\n")
+    if re.findall('[^.\d]', ft) or ft == '.' or ft == '':
+        print("Wrong value, enter a number".upper())
+    else:
+        print(ft, "ft is", round(float(ft) * 30.48, 2), "cm" "\n")
+        while True:
+            another_choise = input("Another value to measure? 'y' for 'Yes', any other key for 'No'\n").lower()
+            if another_choise == "y":
+                break
+            else:
+                print("\nSee ya again!")
+                input("Any key to exit")
+                exit()

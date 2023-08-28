ab340de82c7ab0efc9d176a4fd117a6026b9f0a2
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-28 18:03:18 +0200

    Update converter_mile_km.py

--- converter_mile_km.py
+++ converter_mile_km.py
@@ -4,7 +4,7 @@ while True:
     if re.findall('[^.\d]', mile) or mile == '.' or mile == '':
         print("Wrong value, enter a number".upper())
     else:
-        print(float(mile), "mile is", round(float(mile) / 0.62, 2), "km", "\n")
+        print(mile, "mile is", round(float(mile) / 0.62, 2), "km", "\n")
         while True:
             another_choise = input("Another value to measure? 'y' for 'Yes', any other key for 'No'\n").lower()
             if another_choise == "y":

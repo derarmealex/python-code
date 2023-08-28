95c2468394c33ab2169a8acbde3e34b75209b2ad
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-28 17:13:12 +0200

    Update sum_of_exact_number_of_numbers.py

--- sum_of_exact_number_of_numbers.py
+++ sum_of_exact_number_of_numbers.py
@@ -1,21 +1,13 @@
-def fungus(x, n):
-    a = 0
-    w = 0
-    while w < n:
-        a += x[w]
-        w += 1
-    return a
-print(fungus([1, 2, 3, 4, 5], 3))
+def summus(x, n):
+    summ = 0
+    ix = 0
+    while ix < n:
+        summ += x[ix]
+        ix += 1
+    return summ
+print(summus([1, 2, 3, 4, 5], 3))
 # or
-def fungus(x, n):
-    q = 0
-    for ix, number in enumerate(x):
-        if ix < n:
-            q += number
-    return q
-print(fungus([1, 2, 3, 4, 5], 3))
-# or
-def fungus(x, n):
-    q = [0 + number for ix, number in enumerate(x) if ix < n]
-    return sum(q)
-print(fungus([1, 2, 3, 4, 5], 3))
+def summus(x, n):
+    summ = [0 + number for ix, number in enumerate(x) if ix < n]
+    return sum(summ)
+print(summus([1, 2, 3, 4, 5], 3))

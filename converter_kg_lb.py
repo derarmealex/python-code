baea4302283c6277d85684ff96dbfa9642a69dae
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-28 18:02:43 +0200

    Update converter_kg_lb.py

--- converter_kg_lb.py
+++ converter_kg_lb.py
@@ -4,7 +4,7 @@ while True:
     if re.findall('[^.\d]', kg) or kg == '.' or kg == '':
         print("Wrong value, enter a number".upper())
     else:
-        print(float(kg), "kg is", round(float(kg) * 2.2, 2), "lb", "\n")
+        print(kg, "kg is", round(float(kg) * 2.2, 2), "lb", "\n")
         while True:
             another_choise = input("Another value to measure? 'y' for 'Yes', any other key for 'No'\n").lower()
             if another_choise == "y":

f9315b30111304e6368e445ec177ec5d7e76c618
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-28 18:00:30 +0200

    Add files via upload

---
+++ converter_cm_foot_inch.py
@@ -0,0 +1,20 @@
+import re
+while True:
+    cm = input("Enter cm:\n")
+    if re.findall('[^.\d]', cm) or cm == '.' or cm == '':
+        print("Wrong value, enter a number".upper())
+    else:
+        q = round(float(cm) / 30.48, 2)
+        w = str(q).split('.')[-1]
+        if w == '0':
+            print(cm, "cm is", int(q), "foot", "\n")
+        else:
+            print(cm, "cm is", int(q), "foot", round(int(w) / 12, 2), "inch" "\n")
+        while True:
+            another_choise = input("Another value to measure? 'y' for 'Yes', any other key for 'No'\n").lower()
+            if another_choise == "y":
+                break
+            else:
+                print("\nSee ya again!")
+                input("Any key to exit")
+                exit()

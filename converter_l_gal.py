c77d000eb50d441fb9b5fab25176de03fe70727f
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-28 18:02:59 +0200

    Update converter_l_gal.py

--- converter_l_gal.py
+++ converter_l_gal.py
@@ -4,7 +4,7 @@ while True:
     if re.findall('[^.\d]', l) or l == '.' or l == '':
         print("Wrong value, enter a number".upper())
     else:
-        print(float(l), "l is", round(float(l) * 0.26, 2), "gal", "\n")
+        print(l, "l is", round(float(l) * 0.26, 2), "gal", "\n")
         while True:
             another_choise = input("Another value to measure? 'y' for 'Yes', any other key for 'No'\n").lower()
             if another_choise == "y":

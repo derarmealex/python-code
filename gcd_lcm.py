45d43b14e14fa27833635b556fbb37d118091d02
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-31 17:51:00 +0200

    Update gcd_lcm.py

--- gcd_lcm.py
+++ gcd_lcm.py
@@ -9,9 +9,9 @@ def gcd_lcm(x, y):
             gcd = z
             break
     return (f'{gcd} | {y * x / gcd}')
-print('gcd | lcm of 3, 15 is', gcd_lcm(3, 15))
-print('gcd | lcm of 19, 21 is', gcd_lcm(19, 21))
-print('gcd | lcm of 36, 12 is', gcd_lcm(36, 12))
-print('gcd | lcm of 15, 17 is', gcd_lcm(15, 17))
-print('gcd | lcm of 6, 4 is', gcd_lcm(6, 4))
-print('gcd | lcm of 360, 336 is', gcd_lcm(360, 336))
+print('gcd | lcm of 3, 15 is', gcd_lcm(3, 15))          # gcd | lcm of 3, 15 is 3 | 15.0
+print('gcd | lcm of 19, 21 is', gcd_lcm(19, 21))        # gcd | lcm of 19, 21 is 1 | 399.0
+print('gcd | lcm of 36, 12 is', gcd_lcm(36, 12))        # gcd | lcm of 36, 12 is 12 | 36.0
+print('gcd | lcm of 15, 17 is', gcd_lcm(15, 17))        # gcd | lcm of 15, 17 is 1 | 255.0
+print('gcd | lcm of 6, 4 is', gcd_lcm(6, 4))            # gcd | lcm of 6, 4 is 2 | 12.0
+print('gcd | lcm of 360, 336 is', gcd_lcm(360, 336))    # gcd | lcm of 360, 336 is 24 | 5040.0

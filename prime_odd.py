795382ab2f7cd0519276d09a2cb1241fa2711a14
Alex Green <bartlettgreen@seznam.cz>
2023-09-09 17:55:46 +0200

    Update prime_odd.py

--- prime_odd.py
+++ prime_odd.py
@@ -3,14 +3,14 @@ def find_prime():
     while num < 100:
         if num > 1:
             for i in range(2, num):
-                if (num % i) == 0:
+                if num % i == 0:
                     break
             else:
                 yield num
         num += 1
 def find_odd_prime(seq):
     for num in seq:
-        if (num % 2) != 0:
+        if num % 2 != 0:
             yield num
 a_pipeline = find_odd_prime(find_prime())
 for a_ele in a_pipeline:

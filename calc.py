cb8170d9a6476a13d29ae1331bd596e05c9a79c0
Alex Green <alexander.navotnyi@gmail.com>
2023-09-20 15:31:11 +0200

    routine

---
+++ calc.py
@@ -0,0 +1,44 @@
+from re import findall
+while True:
+    print(
+        'Summation:        "+"\n'
+        'Subtraction:      "-"\n'
+        'Multiplication:   "*"\n'
+        'Division:         "/"\n'
+        'Exponentiation:   "^"\n'
+        '---------------------'
+        )
+    oper = input("Choose the operation: ").strip()
+    if oper not in ('+', '-', '*', '/', '^'):
+        print("\nWrong operator! Try again\n")
+    else:
+        ctr = True
+        while ctr:
+            num1 = input("Enter your first number: ").strip()
+            num2 = input("Enter your second number: ").strip()
+            if findall('[^.\d-]', num1) or findall('[^.\d-]', num2) \
+                    or num1 == '.' or num2 == '.' \
+                    or num1 == '-' or num2 == '-' \
+                    or num1 in '' or num2 in '':
+                print("\nEnter a correct number!")
+                continue
+            elif oper == "+":
+                print(f"{float(num1)} + {float(num2)} = {float(num1) + float(num2)}")
+            elif oper == "-":
+                print(f"{float(num1)} - {float(num2)} = {float(num1) - float(num2)}")
+            elif oper == "*":
+                print(f"{float(num1)} * {float(num2)} = {float(num1) * float(num2)}")
+            elif oper == "/":
+                try:
+                    print(f"{float(num1)} / {float(num2)} = {float(num1) / float(num2)}")
+                except (ZeroDivisionError):
+                    print("couldn't be divided by 0!\n")
+            else:
+                print(f"{float(num1)} ^ {float(num2)} = {float(num1) ** float(num2)}")
+            while True:
+                goon = input("\nLike to do another operation?('y' for 'Yes', any other key for 'No'): \n").lower().strip()
+                if goon == 'y':
+                    ctr = False
+                    break
+                print("\nSee ya again!")
+                exit()

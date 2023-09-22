b49558cd63c0f2c73e65abdb2d5bd39db5815412
Alex Green <alexander.navotnyi@gmail.com>
2023-09-22 18:40:17 +0200

    routine

--- cipher_morse.py
+++ cipher_morse.py
@@ -8,22 +8,25 @@ morse_abc = [
 while True:
     word = input("Enter a word to cypher in Morse code (only letters): ").lower().strip()
 # 'leonardo'
-    word_digit = []
+    if word.isalpha():
+        word_digit = []
 # [11, 4, 14, 13, 0, 17, 3, 14]
-    for letter in word:
-        ix = abc.find(letter)
-        word_digit.append(int(ix))
-#    print(word_digit)
-    final = []
-    for item in word_digit:
-        final.append(morse_abc[item])
-    final = ' '.join([letter for letter in final])
-    print(
-        f"\n  Precyphered : {word}\n"
-        f"  Cyphered    : {final}\n"
-        )
+        for letter in word:
+            ix = abc.find(letter)
+            word_digit.append(int(ix))
+#       print(word_digit)
+        final = []
+        for item in word_digit:
+            final.append(morse_abc[item])
+        final = ' '.join([letter for letter in final])
+        print(
+            f"\n  Precyphered : {word}\n"
+            f"  Cyphered    : {final}\n"
+            )
 # Precyphered : leonardo
 ##Cyphered    : .-.. . --- -. .- .-. -.. ---
+    else:
+        print("\n Use only base letters! \n")
 
 # FROM MORSE ABC TO ABC
 abc = "abcdefghijklmnopqrstuvwxyz"
@@ -37,20 +40,23 @@ while True:
 # '.-.. . --- -. .- .-. -.. ---'
     word_split = word.split()
 # ['.-..', '.', '---', '-.', '.-', '.-.', '-..', '---']
-#    print(word_split)
-    word_digit = []
+#       print(word_split)
+    if set(morse_abc) | set(word_split) == set(morse_abc):
+        word_digit = []
 # [11, 4, 14, 13, 0, 17, 3, 14]
-    for letter in word_split:
-        ix = morse_abc.index(letter)
-        word_digit.append(int(ix))
-#    print(word_digit)
-    final = []
-    for item in word_digit:
-        final.append(abc[item])
-    final = ''.join([letter for letter in final])
-    print(
-        f"\n  Precyphered : {word}\n"
-        f"  Cyphered    : {final}\n"
-        )
+        for letter in word_split:
+            ix = morse_abc.index(letter)
+            word_digit.append(int(ix))
+#       print(word_digit)
+        final = []
+        for item in word_digit:
+            final.append(abc[item])
+        final = ''.join([letter for letter in final])
+        print(
+            f"\n  Precyphered : {word}\n"
+            f"  Cyphered    : {final}\n"
+            )
 # Precyphered : .-.. . --- -. .- .-. -.. ---
 ##Cyphered    : leonardo
+    else:
+        print("\n Use only morse letters! \n")

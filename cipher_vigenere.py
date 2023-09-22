e2bdabf2e6a2727e27538873d08beb8b1fb26678
Alex Green <alexander.navotnyi@gmail.com>
2023-09-22 18:54:33 +0200

    routine

--- cipher_vigenere.py
+++ cipher_vigenere.py
@@ -6,28 +6,28 @@ while True:
 # 1: 'parolchik'                            # 2: 'code'
     if word.isalpha() and pas.isalpha():
         fit_pas = ''
+# 1: parolch                                # 2: codecodeco
         while len(word) > len(fit_pas):
             for letter in pas:
                 fit_pas += letter
         fit_pas = fit_pas[:len(word)]
 #        print(fit_pas)
-# 1: parolch                                # 2: codecodeco
         abc = 'abcdefghijklmnopqrstuvwxyz'
-        move_digit = []
+        digit_move = []
+# 1: [16, 1, 18, 15, 12, 3, 8]              # 2: [3, 15, 4, 5, 3, 15, 4, 5, 3, 15]
         for item in fit_pas:
             move = abc.find(item)
-            move_digit.append(int(move) + 1)
-#        print(move_digit)
-# 1: [16, 1, 18, 15, 12, 3, 8]              # 2: [3, 15, 4, 5, 3, 15, 4, 5, 3, 15]
-        word_digit = []
+            digit_move.append(int(move) + 1)
+#        print(digit_move)
+        digit_word = []
+# 1: [15, 19, 20, 22, 15, 12, 19]           # 2: [19, 21, 4, 14, 21, 2, 9, 18, 15, 13]
         for item in word:
             move = abc.find(item)
-            word_digit.append(int(move) + 1)
-#        print(word_digit)
-# 1: [15, 19, 20, 22, 15, 12, 19]           # 2: [19, 21, 4, 14, 21, 2, 9, 18, 15, 13]
-        cipher = list(map(sum, zip(word_digit, move_digit)))
-#        print(cipher)
+            digit_word.append(int(move) + 1)
+#        print(digit_word)
+        cipher = list(map(sum, zip(digit_word, digit_move)))
 # 1: [31, 20, 38, 37, 27, 15, 27]           # 2: [22, 36, 8, 19, 24, 17, 13, 23, 18, 28]
+#        print(cipher)
         final = []
         for item in cipher:
             item = item - 1
@@ -48,15 +48,23 @@ while True:
 # STANDARD
 while True:
     word = input("Enter a word to cypher     (only letters): ").lower().strip()
+# 1: 'slovtso' ['ostvols']                  # 2: 'moribundus'  ['sudnubirom']
     pas  = input("Enter a password to cypher (only letters): ").lower().strip()
+# 1: 'parolchik'                            # 2: 'code'
     if word.isalpha() and pas.isalpha():
         fit_pas = ""
+# 1: parolch                                # 2: codecodeco
         while len(word) > len(fit_pas):
             for letter in pas:
                 fit_pas += letter
         fit_pas = fit_pas[:len(word)]
+#        print(fit_pas)
         dig_word = [ord(char) - 96 for char in word]
+#1: [19, 12, 15, 22, 20, 19, 15]            # 2: [13, 15, 18, 9, 2, 21, 14, 4, 21, 19]
+#        print(dig_word)
         dig_pas = [ord(char) - 96 for char in fit_pas]
+# 1: [16, 1, 18, 15, 12, 3, 8]              # 2: [3, 15, 4, 5, 3, 15, 4, 5, 3, 15]
+#        print(dig_pas)
         cipher = list(map(sum, zip(dig_word, dig_pas)))
         final = (''.join([chr(char + 96) if char + 96 < 123 else chr(char + 96 - 26) for char in cipher]))
         print(
@@ -67,3 +75,5 @@ while True:
 # 1: Precyphered     : slovtso               # 2: Precyphered     : moribundus
 ##   Cyphered        : imgkfvw               ##   Cyphered        : pdvnejrixh
 ##   Cypher(password): parolchik             ##   Cypher(password): code
+    else:
+        print("\n Use only base letters! \n")

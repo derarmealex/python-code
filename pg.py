91f6bfe31f73254ec9f25f54f07cedf5b302cdd1
Alex Green <alexander.navotnyi@gmail.com>
2023-09-23 20:54:54 +0200

    routine

---
+++ pg.py
@@ -0,0 +1,39 @@
+a = "oqrcvmes"
+b = "0304071115"
+for x, y in zip(a, b): print(x + y, end='')
+"""
+def pp(x):
+    for ix, item in stg:
+        if item in x:
+            final.append(ix + move)
+
+
+while True:
+    move = 3
+    stg = 'abcdefghijklmnopqrstuvwxyz'
+    stg = enumerate(stg)
+    final = []
+    pp(input('Enter a word...  ').lower().strip())
+    q = str(bytearray(final))
+    print(q[len("bytearray(b'"):-2])
+"""
+# or
+move = 3
+abc = 'abcdefghijklmnopqrstuvwxyz'
+abc = enumerate(abc)
+while True:
+    word = input('Enter a word...  ').lower().strip()
+    if word.isalpha():
+        word_digit = []
+        for ix, letter in abc:
+            if letter in word:
+                word_digit.append(ix + move)
+#       print(word_digit)
+        cyph_word = str(bytearray(word_digit))
+#       print(cyph_word)
+#       q = cyph_word[len("bytearray(b'"):-2]
+#       print(q)
+        cyph_word = ''.join([num for num in cyph_word if num.isnumeric()])
+        print(cyph_word)
+    else:
+        print("\n\tUse only base letters!\n")

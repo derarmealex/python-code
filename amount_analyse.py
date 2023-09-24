28f19ff71535f0b68cc0621a0792a5f1f4be2549
Alex Green <alexander.navotnyi@gmail.com>
2023-09-24 21:35:21 +0200

    routine

--- amount_analyse.py
+++ amount_analyse.py
@@ -3,27 +3,27 @@ while True:
     if amount.isnumeric():
         amount = int(amount)
         #print(amount)                       # 123579
-        d50 = amount // 50                  # 2471
+        d50 = amount // 50
         print("\t50 cent ==>", d50, "pc.")
         amount = amount % 50                # 29
         #print(amount)
-        d20 = amount // 20                  # 1
+        d20 = amount // 20
         print("\t20 cent ==>", d20, "pc.")
         amount = amount % 20                # 9
         #print(amount)
-        d10 = amount // 10                  # 0
+        d10 = amount // 10
         print("\t10 cent ==>", d10, "pc.")
         amount = amount % 10                # 9
         #print(amount)
-        d5 = amount // 5                    # 1
+        d5 = amount // 5
         print("\t 5 cent ==>", d5, "pc.")
         amount = amount % 5                 # 4
         #print(amount)
-        d2 = amount // 2                    # 2
+        d2 = amount // 2
         print("\t 2 cent ==>", d2, "pc.")
         amount = amount % 2                 # 0
         #print(amount)
-        d1 = amount // 1                    # 0
+        d1 = amount // 1
         print("\t 1 cent ==>", d1, "pc.\n")
     else:
         print("\n\tEnter a number!\n")

f5cb41dfbb8b624f2e92779df41a07a573fce817
Alex Green <alexander.navotnyi@gmail.com>
2023-09-29 23:04:21 +0200

    routine

--- bmi.py
+++ bmi.py
@@ -1,3 +1,33 @@
+while True:
+    try:
+        m = float(input("Enter mass of body in kg   : "))
+        h = float(input("Enter height of body in cm : "))
+    except ValueError:
+        print("\n\tEnter a correct number!\n")
+    else:
+        try:
+            calc = round(m / (h / 100) ** 2, 2)
+        except (ZeroDivisionError):
+            print("\n\tCouldn't be divided by 0! Enter a correct number\n")
+        else:
+            print("\n\tBMI is:", calc)
+            if calc <= 16.0:
+                print("\tIt's underweight (severe thinness)\n")
+            elif 16.0 < calc < 16.9:
+                print("\tIt's underweight (moderate thinness)\n")
+            elif 17.0 < calc < 18.4:
+                print("\tIt's underweight (mild thinness)\n")
+            elif 18.5 < calc < 24.9:
+                print("\tIt's a normal range\n")
+            elif 25.0 < calc < 29.9:
+                print("\tIt's overweight (pre-obese)\n")
+            elif 30.0 < calc < 34.9:
+                print("\tIt's obese (class I)\n")
+            elif 35.0 < calc < 39.9:
+                print("\tIt's obese (class I)\n")
+            elif calc >= 40.0:
+                print("\tIt's obese (class I)\n")
+# or
 from re import findall
 while True:
     m = input("Enter mass of body in kg: ").strip()
@@ -26,3 +56,9 @@ while True:
             print("It's obese (class I)\n")
         elif calc >= 40.0:
             print("It's obese (class I)\n")
+# INPUT
+#       Enter mass of body in kg   : 90
+#       Enter height of body in cm : 180
+# OUTPUT
+#       BMI is: 27.78
+#       It's overweight (pre-obese)
--- calc.py
+++ calc.py
@@ -1,44 +1,89 @@
+while True:
+    print(
+        'Summation:             "+"\n'
+        'Subtraction:           "-"\n'
+        'Multiplication:        "*"\n'
+        'Division:              "/"\n'
+        'Exponentiation:        "^"\n'
+        '--------------------------'
+        )
+    oper = input("Choose the operation     : ")
+    try:
+        num1 = float(input("Enter your first number  : "))
+        num2 = float(input("Enter your second number : "))
+    except ValueError:
+        print("\n\tEnter a correct number!\n")
+        continue
+    match oper:
+        case "+":
+            print(f"\t{float(num1)} + {float(num2)} = {float(num1) + float(num2)}\n")
+        case "-":
+            print(f"\t{float(num1)} - {float(num2)} = {float(num1) - float(num2)}\n")
+        case "*":
+            print(f"\t{float(num1)} * {float(num2)} = {float(num1) * float(num2)}\n")
+        case "/":
+            try:
+                print(f"\t{float(num1)} / {float(num2)} = {float(num1) / float(num2)}\n")
+            except (ZeroDivisionError):
+                print("\n\tCouldn't be divided by 0! Enter a correct number\n")
+        case "^":
+            print(f"\t{float(num1)} ^ {float(num2)} = {float(num1) ** float(num2)}\n")
+        case _:
+            print(f"\n\tWrong operator! --> ({oper}) <-- Try again\n")
+# or
 from re import findall
 while True:
     print(
-        'Summation:        "+"\n'
-        'Subtraction:      "-"\n'
-        'Multiplication:   "*"\n'
-        'Division:         "/"\n'
-        'Exponentiation:   "^"\n'
-        '---------------------'
+        'Summation:             "+"\n'
+        'Subtraction:           "-"\n'
+        'Multiplication:        "*"\n'
+        'Division:              "/"\n'
+        'Exponentiation:        "^"\n'
+        '--------------------------'
         )
-    oper = input("Choose the operation: ").strip()
-    if oper not in ('+', '-', '*', '/', '^'):
-        print("\nWrong operator! Try again\n")
-    else:
-        ctr = True
-        while ctr:
-            num1 = input("Enter your first number: ").strip()
-            num2 = input("Enter your second number: ").strip()
-            if findall('[^.\d-]', num1) or findall('[^.\d-]', num2) \
-                    or num1 == '.' or num2 == '.' \
-                    or num1 == '-' or num2 == '-' \
-                    or num1 in '' or num2 in '':
-                print("\nEnter a correct number!")
-                continue
-            elif oper == "+":
-                print(f"{float(num1)} + {float(num2)} = {float(num1) + float(num2)}")
-            elif oper == "-":
-                print(f"{float(num1)} - {float(num2)} = {float(num1) - float(num2)}")
-            elif oper == "*":
-                print(f"{float(num1)} * {float(num2)} = {float(num1) * float(num2)}")
-            elif oper == "/":
-                try:
-                    print(f"{float(num1)} / {float(num2)} = {float(num1) / float(num2)}")
-                except (ZeroDivisionError):
-                    print("couldn't be divided by 0!\n")
-            else:
-                print(f"{float(num1)} ^ {float(num2)} = {float(num1) ** float(num2)}")
-            while True:
-                goon = input("\nLike to do another operation?('y' for 'Yes', any other key for 'No'): \n").lower().strip()
-                if goon == 'y':
-                    ctr = False
-                    break
-                print("\nSee ya again!")
-                exit()
+    oper = input("Choose the operation     : ").strip()
+    while oper:
+        num1 = input("Enter your first number  : ").strip()
+        num2 = input("Enter your second number : ").strip()
+        if findall('[^.\d-]', num1) or findall('[^.\d-]', num2) \
+                or num1 == '.' or num2 == '.' \
+                or num1 == '-' or num2 == '-' \
+                or num1 in '' or num2 in '':
+            print("\n\tEnter a correct number!\n")
+            continue
+        elif oper == "+":
+            print(f"\t{float(num1)} + {float(num2)} = {float(num1) + float(num2)}\n")
+        elif oper == "-":
+            print(f"\t{float(num1)} - {float(num2)} = {float(num1) - float(num2)}\n")
+        elif oper == "*":
+            print(f"\t{float(num1)} * {float(num2)} = {float(num1) * float(num2)}\n")
+        elif oper == "/":
+            try:
+                print(f"\t{float(num1)} / {float(num2)} = {float(num1) / float(num2)}\n")
+            except (ZeroDivisionError):
+                print("\n\tCouldn't be divided by 0! Enter a correct number\n")
+        elif oper == "^":
+            print(f"\t{float(num1)} ^ {float(num2)} = {float(num1) ** float(num2)}\n")
+        else:
+            print(f"\n\tWrong operator! --> ({oper}) <-- Try again\n")
+        while True:
+            goon = input("\nLike to do another operation?('y' for 'Yes', any other key for 'No'): ").lower().strip()
+            if goon == 'y':
+                oper = False
+                break
+            print("\n\tSee ya again!")
+            exit()
+# INPUT
+#       Summation:             "+"
+#       Subtraction:           "-"
+#       Multiplication:        "*"
+#       Division:              "/"
+#       Exponentiation:        "^"
+#       --------------------------
+#       Choose the operation     : ^
+#       Enter your first number  : 3
+#       Enter your second number : 2
+# OUTPUT
+#       3.0 ^ 2.0 = 9.0
+#       Like to do another operation?('y' for 'Yes', any other key for 'No'): n
+#       See ya again!

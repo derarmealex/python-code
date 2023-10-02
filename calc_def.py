69ba18e0bf0913837bbf607e7a29bcb256cef2a1
Alex Green <alexander.navotnyi@gmail.com>
2023-10-02 18:19:45 +0200

    routine

--- calc_def.py
+++ calc_def.py
@@ -50,44 +50,201 @@ def expon(x, n):
 # -3 ** 2 = 0.125
 
 
-# 1.0 + 2.0 ** 2 --3.0 * 0.1 / 99.0 ** 2.5
-collection = [1.0, '+', 2.0, '^', 2.0, '-', -3.0, '*', 0.1, '/', 99.0, '^', 2.5]
+# 9.0 ^ 2.0 + 1 - 3.5 * 0.0 / 2.0 ^ 5.0 * 7.0 * 10.0 = ?
+collection = []
+# [9.0, '^', 2.0, '+', 1, '-', 3.5, '*', 0.0, '/', 2.0, '^', 5.0, '*', 7.0, '*', 10.0]
+main_ctr = True
+while main_ctr:
+    try:
+        num = float(input("Enter [next] number to calculate: "))
+        print("")
+    except ValueError:
+        print(f"\n\tIncorrect number! Try again")
+        print("\tYour collection to process:", collection)
+        continue
+    else:
+        collection.append(num)
+#        print("\n\tYour collection to process:", collection, "\n")
+        while True:
+            print("\tYour collection to process:", collection, "")
+            oper = input(
+                        'Summation     : "+"\n'
+                        'Subtraction   : "-"\n'
+                        'Multiplication: "*"\n'
+                        'Division      : "/"\n'
+                        'Exponentiation: "^"\n'
+                        '-------------------\n'
+                        "Enter operator, or print 'start' to break input and start calculating: "
+                        ).strip().lower()
+            print("")
+            if oper == "+" or oper == "-" or oper == "*" or oper == "/" or oper == "^":
+                collection.append(oper)
+                print("\tYour collection to process now:", collection, "\n")
+                break
+            elif oper == "start":
+                main_ctr = False
+                break
+            else:
+                print(f"\tWrong operator! --> ({oper}) <-- Try again")
+fin_col = " ".join([str(x) for x in collection])
+# 9.0 ^ 2.0 + 1 - 3.5 * 0.0 / 2.0 ^ 5.0 * 7.0 * 10.0
+#print(fin_col)
 col = collection
-copy_col = col.copy()
-# [(0, 1.0), (1, '+'), (2, 2.0), (3, '^'), (4, 2.0), (5, '-'), (6, -3.0), (7, '*'), (8, 0.1), (9, '/'), (10, 99.0), (11, '^'), (12, 2.5)]
+# [9.0, '^', 2.0, '+', 1, '-', 3.5, '*', 0.0, '/', 2.0, '^', 5.0, '*', 7.0, '*', 10.0]
+#print(col)
+#print(list(enumerate(col)))
+# 1: [(0, 9.0), (1, '^'), (2, 2.0), (3, '+'), (4, 1), (5, '-'), (6, 3.5), (7, '*'), (8, 0.0),
+# (9, '/'), (10, 2.0), (11, '^'), (12, 5.0), (13, '*'), (14, 7.0), (15, '*'), (16, 10.0)]
+# 2: [(0, 81.0), (1, '+'), (2, 1), (3, '-'), (4, 3.5), (5, '*'), (6, 0.0), (7, '/'),
+# (8, 2.0), (9, '^'), (10, 5.0), (11, '*'), (12, 7.0), (13, '*'), (14, 10.0)]
+index_move = 0
 if "^" in col:
-    step = 0
     for ix, item in enumerate(col):
-        if item == "^":                 # (3, '^')  #(11, '^')
-#            print(ix)
-            p = col[ix - 1]             # 2.0       # 99.0
-#            print(p)
-            ow = col[ix + 1]            # 2.0       # 2.5
-#            print(ow)
-# [1.0, '+', 2.0, '^', 2.0, '-', -3.0, '*', 0.1, '/', 99.0, '^', 2.5]
-# [1.0, '+', 4.0, '-', -3.0, '*', 0.1, '/', 99.0, '^', 2.5]
-#            print(copy_col)
-            temp_slice = copy_col[:ix-step]
-            temp_slice2 = copy_col[ix+2-step:]
-            copy_col = temp_slice + temp_slice2
-# [1.0, '+', 2.0, '-', -3.0, '*', 0.1, '/', 99.0, '^', 2.5]
-# [1.0, '+', 4.0, '-', -3.0, '*', 0.1, '/', 99.0]
-#            print(copy_col)
-            pow_res = expon(p, ow)      # 4.0       # 97518.71871081983
+        if item == "^":                         # 1: (1, '^')   2: (9, '^')
+#            print(list(enumerate(col)))
+            ix -= index_move
+#            print(ix)                           # 1: 1          2: 9
+            num1 = col[ix - 1]                  # 1: 9.0        2: 2.0
+#            print(num1)
+            num2 = col[ix + 1]                  # 1: 2.0        2: 5.0
+#            print(num2)
+# 1: [9.0, '^', 2.0, '+', 1, '-', 3.5, '*', 0.0, '/', 2.0, '^', 5.0, '*', 7.0, '*', 10.0]
+# 2: [81.0, '+', 1, '-', 3.5, '*', 0.0, '/', 2.0, '^', 5.0, '*', 7.0, '*', 10.0]
+#            print(col)
+            pow_res = expon(num1, num2)         # 1: 81.0       2: 32.0
 #            print(pow_res)
-            copy_col[ix - 1-step] = pow_res
-# [1.0, '+', 4.0, '-', -3.0, '*', 0.1, '/', 99.0, '^', 2.5]
-# [1.0, '+', 4.0, '-', -3.0, '*', 0.1, '/', 97518.71871081983]
-#            print(copy_col)
-            step += 2
-print(copy_col)
-
+            col[ix - 1] = pow_res
+# 1: [81.0, '^', 2.0, '+', 1, '-', 3.5, '*', 0.0, '/', 2.0, '^', 5.0, '*', 7.0, '*', 10.0]
+# 2: [81.0, '+', 1, '-', 3.5, '*', 0.0, '/', 32.0, '^', 5.0, '*', 7.0, '*', 10.0]
+#            print(col)
+            temp_slice = col[:ix]
+            temp_slice2 = col[ix + 2:]
+            col = temp_slice + temp_slice2
+# 1: [81.0, '+', 1, '-', 3.5, '*', 0.0, '/', 2.0, '^', 5.0, '*', 7.0, '*', 10.0]
+# 2: [81.0, '+', 1, '-', 3.5, '*', 0.0, '/', 32.0, '*', 7.0, '*', 10.0]
+#            print(col)
+            index_move += 2
+#print(list(enumerate(col)))
+# [(0, 81.0), (1, '+'), (2, 1), (3, '-'), (4, 3.5), (5, '*'), (6, 0.0),
+# (7, '/'), (8, 32.0), (9, '*'), (10, 7.0), (11, '*'), (12, 10.0)]
+index_move = 0
 if "/" in col:
     for ix, item in enumerate(col):
-        if item == "^":
-            p = col[ix - 1]             # 2.0   # 99.0
-#if *
-
-
-print(col)
-# [1.0, '+', 2.0, '^', 2.0, '-', -3.0, '*', 0.1, '/', 99.0, '^', 2.5]
+        if item == "/":                         # (7, '/')
+#            print(list(enumerate(col)))
+            ix -= index_move
+#            print(ix)                           # 7
+            num1 = col[ix - 1]                  # 0.0
+#            print(num1)
+            num2 = col[ix + 1]                  # 32.0
+#            print(num2)
+# [81.0, '+', 1, '-', 3.5, '*', 0.0, '/', 32.0, '*', 7.0, '*', 10.0]
+#            print(col)
+            try:
+                div_res = divis(num1, num2)     # 0.0
+            except ZeroDivisionError:
+                print("\n\tCouldn't be divided by 0! Expression has no solution\n")
+                exit()
+#            print(div_res)
+            col[ix - 1] = div_res
+# [81.0, '+', 1, '-', 3.5, '*', 0.0, '/', 32.0, '*', 7.0, '*', 10.0]
+#            print(col)
+            temp_slice = col[:ix]
+            temp_slice2 = col[ix + 2:]
+            col = temp_slice + temp_slice2
+# [81.0, '+', 1, '-', 3.5, '*', 0.0, '*', 7.0, '*', 10.0]
+#            print(col)
+            index_move += 2
+#print(list(enumerate(col)))
+# 1: [(0, 81.0), (1, '+'), (2, 1), (3, '-'), (4, 3.5), (5, '*'),
+# (6, 0.0), (7, '*'), (8, 7.0), (9, '*'), (10, 10.0)]
+# 2: [(0, 81.0), (1, '+'), (2, 1), (3, '-'), (4, 0.0), (5, '*'),
+# (6, 7.0), (7, '*'), (8, 10.0)]
+# 3: [(0, 81.0), (1, '+'), (2, 1), (3, '-'), (4, 0.0), (5, '*'), (6, 10.0)]
+index_move = 0
+if "*" in col:
+    for ix, item in enumerate(col):
+        if item == "*":                         # 1: (5, '*')  2: (5, '*')  3: (5, '*')
+#            print(list(enumerate(col)))
+            ix -= index_move
+#            print(ix)                           # 1: 5         2: 5         3: 5
+            num1 = col[ix - 1]                  # 1: 3.5       2: 0.0       3: 0.0
+#            print(num1)
+            num2 = col[ix + 1]                  # 1: 0.0       2: 7.0       3: 10.0
+#            print(num2)
+# 1: [81.0, '+', 1, '-', 3.5, '*', 0.0, '*', 7.0, '*', 10.0]
+# 2: [81.0, '+', 1, '-', 0.0, '*', 7.0, '*', 10.0]
+# 3: [81.0, '+', 1, '-', 0.0, '*', 10.0]
+#            print(col)
+            mult_res = mult(num1, num2)         # 1: 0.0       2: 0.0       3: 0.0
+#            print(mult_res)
+            col[ix - 1] = mult_res
+# 1: [81.0, '+', 1, '-', 0.0, '*', 0.0, '*', 7.0, '*', 10.0]
+# 2: [81.0, '+', 1, '-', 0.0, '*', 7.0, '*', 10.0]
+# 3: [81.0, '+', 1, '-', 0.0, '*', 10.0]
+#            print(col)
+            temp_slice = col[:ix]
+            temp_slice2 = col[ix + 2:]
+            col = temp_slice + temp_slice2
+# 1: [81.0, '+', 1, '-', 0.0, '*', 7.0, '*', 10.0]
+# 2: [81.0, '+', 1, '-', 0.0, '*', 10.0]
+# 3: [81.0, '+', 1, '-', 0.0]
+#            print(col)
+            index_move += 2
+#print(list(enumerate(col)))
+# [(0, 81.0), (1, '+'), (2, 1), (3, '-'), (4, 0.0)]
+index_move = 0
+if "-" in col:
+    for ix, item in enumerate(col):
+        if item == "-":                         # (3, '-')
+#            print(list(enumerate(col)))
+            ix -= index_move
+#            print(ix)                           # 3
+            num1 = col[ix - 1]                  # 1
+#            print(num1)
+            num2 = col[ix + 1]                  # 0.0
+#            print(num2)
+# [81.0, '+', 1, '-', 0.0]
+#            print(col)
+            sub_res = minus(num1, num2)         # 0.0
+#            print(sub_res)
+            col[ix - 1] = sub_res
+# [81.0, '+', 1.0, '-', 0.0]
+#            print(col)
+            temp_slice = col[:ix]
+            temp_slice2 = col[ix + 2:]
+            col = temp_slice + temp_slice2
+# [81.0, '+', 1.0]
+#            print(col)
+            index_move += 2
+#print(list(enumerate(col)))
+# [(0, 81.0), (1, '+'), (2, 1.0)]
+index_move = 0
+if "+" in col:
+    for ix, item in enumerate(col):
+        if item == "+":                         # (1, '+')
+#            print(list(enumerate(col)))
+            ix -= index_move
+#            print(ix)                           # 1
+            num1 = col[ix - 1]                  # 81.0
+#            print(num1)
+            num2 = col[ix + 1]                  # 1.0
+#            print(num2)
+# [81.0, '+', 1.0]
+#            print(col)
+            sum_res = plus(num1, num2)          # 82.0
+#            print(sum_res)
+            col[ix - 1] = sum_res
+# [82.0, '+', 1.0]
+#            print(col)
+            temp_slice = col[:ix]
+            temp_slice2 = col[ix + 2:]
+            col = temp_slice + temp_slice2
+# [82.0]
+#            print(col)
+            index_move += 2
+#print(list(enumerate(col)))
+# [(0, 82.0)]
+result = col[0]                                 # [82.0]
+print(f"\n\t{fin_col} = {result}\n")
+# 9.0 ^ 2.0 + 1 - 3.5 * 0.0 / 2.0 ^ 5.0 * 7.0 * 10.0 = 82.0
--- calc_raw.py
+++ calc_raw.py
@@ -1,5 +1,6 @@
+# 9.0 ^ 2.0 + 1 - 3.5 * 0.0 / 2.0 ^ 5.0 * 7.0 * 10.0 = ?
 collection = []
-# [1.0, '+', 2.0, '^', 2.0, '-', -3.0, '*', 0.1, '/', 99.0, '^', 2.5]
+# [9.0, '^', 2.0, '+', 1, '-', 3.5, '*', 0.0, '/', 2.0, '^', 5.0, '*', 7.0, '*', 10.0]
 main_ctr = True
 while main_ctr:
     try:
@@ -33,53 +34,171 @@ while main_ctr:
                 break
             else:
                 print(f"\tWrong operator! --> ({oper}) <-- Try again")
-#print("\tYour collection to process:", collection)
-# Your collection to process: [1.0, '+', 2.0, '^', 2.0, '-', -3.0, '*', 0.1, '/', 99.0, '^', 2.5]
 fin_col = " ".join([str(x) for x in collection])
-print(fin_col)
-# 1.0 + 2.0 ^ 2.0 - -3.0 * 0.1 / 99.0 ^ 2.5
-collection = [1.0, '+', 2.0, '^', 2.0, '-', -3.0, '*', 0.1, '/', 99.0, '^', 2.5]
+# 9.0 ^ 2.0 + 1 - 3.5 * 0.0 / 2.0 ^ 5.0 * 7.0 * 10.0
+#print(fin_col)
 col = collection
-copy_col = col.copy()
-# [(0, 1.0), (1, '+'), (2, 2.0), (3, '^'), (4, 2.0), (5, '-'), (6, -3.0), (7, '*'), (8, 0.1), (9, '/'), (10, 99.0), (11, '^'), (12, 2.5)]
+# [9.0, '^', 2.0, '+', 1, '-', 3.5, '*', 0.0, '/', 2.0, '^', 5.0, '*', 7.0, '*', 10.0]
+#print(col)
+#print(list(enumerate(col)))
+# 1: [(0, 9.0), (1, '^'), (2, 2.0), (3, '+'), (4, 1), (5, '-'), (6, 3.5), (7, '*'), (8, 0.0),
+# (9, '/'), (10, 2.0), (11, '^'), (12, 5.0), (13, '*'), (14, 7.0), (15, '*'), (16, 10.0)]
+# 2: [(0, 81.0), (1, '+'), (2, 1), (3, '-'), (4, 3.5), (5, '*'), (6, 0.0), (7, '/'),
+# (8, 2.0), (9, '^'), (10, 5.0), (11, '*'), (12, 7.0), (13, '*'), (14, 10.0)]
+index_move = 0
 if "^" in col:
-    step = 0
     for ix, item in enumerate(col):
-        if item == "^":                 # 1: (3, '^')   # 2: (11, '^')
-#            print(ix)                  # 1: 3          # 2: 11
-            p = col[ix - 1]             # 1: 2.0        # 2: 99.0
-#            print(p)
-            ow = col[ix + 1]            # 1: 2.0        # 2: 2.5
-#            print(ow)
-# 1: [1.0, '+', 2.0, '^', 2.0, '-', -3.0, '*', 0.1, '/', 99.0, '^', 2.5]
-# 2: [1.0, '+', 4.0, '-', -3.0, '*', 0.1, '/', 99.0, '^', 2.5]
-#            print(copy_col)
-            temp_slice = copy_col[:ix-step]
-            temp_slice2 = copy_col[ix+2-step:]
-            copy_col = temp_slice + temp_slice2
-# 1: [1.0, '+', 2.0, '-', -3.0, '*', 0.1, '/', 99.0, '^', 2.5]
-# 2: [1.0, '+', 4.0, '-', -3.0, '*', 0.1, '/', 99.0]
-#            print(copy_col)
-            pow_res = p ** ow           # 4.0       # 97518.71871081983
+        if item == "^":                         # 1: (1, '^')   2: (9, '^')
+#            print(list(enumerate(col)))
+            ix -= index_move
+#            print(ix)                           # 1: 1          2: 9
+            num1 = col[ix - 1]                  # 1: 9.0        2: 2.0
+#            print(num1)
+            num2 = col[ix + 1]                  # 1: 2.0        2: 5.0
+#            print(num2)
+# 1: [9.0, '^', 2.0, '+', 1, '-', 3.5, '*', 0.0, '/', 2.0, '^', 5.0, '*', 7.0, '*', 10.0]
+# 2: [81.0, '+', 1, '-', 3.5, '*', 0.0, '/', 2.0, '^', 5.0, '*', 7.0, '*', 10.0]
+#            print(col)
+            pow_res = num1 ** num2              # 1: 81.0       2: 32.0
 #            print(pow_res)
-            copy_col[ix - 1-step] = pow_res
-# 1: [1.0, '+', 4.0, '-', -3.0, '*', 0.1, '/', 99.0, '^', 2.5]
-# 2: [1.0, '+', 4.0, '-', -3.0, '*', 0.1, '/', 97518.71871081983]
-#            print(copy_col)
-            step += 2
-print(copy_col)
-
+            col[ix - 1] = pow_res
+# 1: [81.0, '^', 2.0, '+', 1, '-', 3.5, '*', 0.0, '/', 2.0, '^', 5.0, '*', 7.0, '*', 10.0]
+# 2: [81.0, '+', 1, '-', 3.5, '*', 0.0, '/', 32.0, '^', 5.0, '*', 7.0, '*', 10.0]
+#            print(col)
+            temp_slice = col[:ix]
+            temp_slice2 = col[ix + 2:]
+            col = temp_slice + temp_slice2
+# 1: [81.0, '+', 1, '-', 3.5, '*', 0.0, '/', 2.0, '^', 5.0, '*', 7.0, '*', 10.0]
+# 2: [81.0, '+', 1, '-', 3.5, '*', 0.0, '/', 32.0, '*', 7.0, '*', 10.0]
+#            print(col)
+            index_move += 2
+#print(list(enumerate(col)))
+# [(0, 81.0), (1, '+'), (2, 1), (3, '-'), (4, 3.5), (5, '*'), (6, 0.0),
+# (7, '/'), (8, 32.0), (9, '*'), (10, 7.0), (11, '*'), (12, 10.0)]
+index_move = 0
 if "/" in col:
     for ix, item in enumerate(col):
-        if item == "^":
-            p = col[ix - 1]             # 2.0   # 99.0
-#if *
-
-
-print(col)
-# [1.0, '+', 2.0, '^', 2.0, '-', -3.0, '*', 0.1, '/', 99.0, '^', 2.5]
+        if item == "/":                         # (7, '/')
+#            print(list(enumerate(col)))
+            ix -= index_move
+#            print(ix)                           # 7
+            num1 = col[ix - 1]                  # 0.0
+#            print(num1)
+            num2 = col[ix + 1]                  # 32.0
+#            print(num2)
+# [81.0, '+', 1, '-', 3.5, '*', 0.0, '/', 32.0, '*', 7.0, '*', 10.0]
+#            print(col)
+            try:
+                div_res = num1 / num2              # 0.0
+            except ZeroDivisionError:
+                print("\n\tCouldn't be divided by 0! Expression has no solution\n")
+                exit()
+#            print(div_res)
+            col[ix - 1] = div_res
+# [81.0, '+', 1, '-', 3.5, '*', 0.0, '/', 32.0, '*', 7.0, '*', 10.0]
+#            print(col)
+            temp_slice = col[:ix]
+            temp_slice2 = col[ix + 2:]
+            col = temp_slice + temp_slice2
+# [81.0, '+', 1, '-', 3.5, '*', 0.0, '*', 7.0, '*', 10.0]
+#            print(col)
+            index_move += 2
+#print(list(enumerate(col)))
+# 1: [(0, 81.0), (1, '+'), (2, 1), (3, '-'), (4, 3.5), (5, '*'),
+# (6, 0.0), (7, '*'), (8, 7.0), (9, '*'), (10, 10.0)]
+# 2: [(0, 81.0), (1, '+'), (2, 1), (3, '-'), (4, 0.0), (5, '*'),
+# (6, 7.0), (7, '*'), (8, 10.0)]
+# 3: [(0, 81.0), (1, '+'), (2, 1), (3, '-'), (4, 0.0), (5, '*'), (6, 10.0)]
+index_move = 0
+if "*" in col:
+    for ix, item in enumerate(col):
+        if item == "*":                         # 1: (5, '*')  2: (5, '*')  3: (5, '*')
+#            print(list(enumerate(col)))
+            ix -= index_move
+#            print(ix)                           # 1: 5         2: 5         3: 5
+            num1 = col[ix - 1]                  # 1: 3.5       2: 0.0       3: 0.0
+#            print(num1)
+            num2 = col[ix + 1]                  # 1: 0.0       2: 7.0       3: 10.0
+#            print(num2)
+# 1: [81.0, '+', 1, '-', 3.5, '*', 0.0, '*', 7.0, '*', 10.0]
+# 2: [81.0, '+', 1, '-', 0.0, '*', 7.0, '*', 10.0]
+# 3: [81.0, '+', 1, '-', 0.0, '*', 10.0]
+#            print(col)
+            mult_res = num1 * num2              # 1: 0.0       2: 0.0       3: 0.0
+#            print(mult_res)
+            col[ix - 1] = mult_res
+# 1: [81.0, '+', 1, '-', 0.0, '*', 0.0, '*', 7.0, '*', 10.0]
+# 2: [81.0, '+', 1, '-', 0.0, '*', 7.0, '*', 10.0]
+# 3: [81.0, '+', 1, '-', 0.0, '*', 10.0]
+#            print(col)
+            temp_slice = col[:ix]
+            temp_slice2 = col[ix + 2:]
+            col = temp_slice + temp_slice2
+# 1: [81.0, '+', 1, '-', 0.0, '*', 7.0, '*', 10.0]
+# 2: [81.0, '+', 1, '-', 0.0, '*', 10.0]
+# 3: [81.0, '+', 1, '-', 0.0]
+#            print(col)
+            index_move += 2
+#print(list(enumerate(col)))
+# [(0, 81.0), (1, '+'), (2, 1), (3, '-'), (4, 0.0)]
+index_move = 0
+if "-" in col:
+    for ix, item in enumerate(col):
+        if item == "-":                         # (3, '-')
+#            print(list(enumerate(col)))
+            ix -= index_move
+#            print(ix)                           # 3
+            num1 = col[ix - 1]                  # 1
+#            print(num1)
+            num2 = col[ix + 1]                  # 0.0
+#            print(num2)
+# [81.0, '+', 1, '-', 0.0]
+#            print(col)
+            sub_res = num1 - num2               # 0.0
+#            print(sub_res)
+            col[ix - 1] = sub_res
+# [81.0, '+', 1.0, '-', 0.0]
+#            print(col)
+            temp_slice = col[:ix]
+            temp_slice2 = col[ix + 2:]
+            col = temp_slice + temp_slice2
+# [81.0, '+', 1.0]
+#            print(col)
+            index_move += 2
+#print(list(enumerate(col)))
+# [(0, 81.0), (1, '+'), (2, 1.0)]
+index_move = 0
+if "+" in col:
+    for ix, item in enumerate(col):
+        if item == "+":                         # (1, '+')
+#            print(list(enumerate(col)))
+            ix -= index_move
+#            print(ix)                           # 1
+            num1 = col[ix - 1]                  # 81.0
+#            print(num1)
+            num2 = col[ix + 1]                  # 1.0
+#            print(num2)
+# [81.0, '+', 1.0]
+#            print(col)
+            sum_res = num1 + num2               # 82.0
+#            print(sum_res)
+            col[ix - 1] = sum_res
+# [82.0, '+', 1.0]
+#            print(col)
+            temp_slice = col[:ix]
+            temp_slice2 = col[ix + 2:]
+            col = temp_slice + temp_slice2
+# [82.0]
+#            print(col)
+            index_move += 2
+#print(list(enumerate(col)))
+# [(0, 82.0)]
+result = col[0]                                 # [82.0]
+print(f"\n\t{fin_col} = {result}\n")
+# 9.0 ^ 2.0 + 1 - 3.5 * 0.0 / 2.0 ^ 5.0 * 7.0 * 10.0 = 82.0
 
 # ANCIENT
+"""
 while True:
     print(
         'Summation:             "+"\n'
@@ -106,7 +225,7 @@ while True:
         case "/":
             try:
                 print(f"\t{float(num1)} / {float(num2)} = {float(num1) / float(num2)}\n")
-            except (ZeroDivisionError):
+            except ZeroDivisionError:
                 print("\n\tCouldn't be divided by 0! Enter a correct number\n")
         case "^":
             print(f"\t{float(num1)} ^ {float(num2)} = {float(num1) ** float(num2)}\n")
@@ -142,7 +261,7 @@ while True:
         elif oper == "/":
             try:
                 print(f"\t{float(num1)} / {float(num2)} = {float(num1) / float(num2)}\n")
-            except (ZeroDivisionError):
+            except ZeroDivisionError:
                 print("\n\tCouldn't be divided by 0! Enter a correct number\n")
         elif oper == "^":
             print(f"\t{float(num1)} ^ {float(num2)} = {float(num1) ** float(num2)}\n")
@@ -169,3 +288,4 @@ while True:
 #       3.0 ^ 2.0 = 9.0
 #       Like to do another operation?('y' for 'Yes', any other key for 'No'): n
 #       See ya again!
+"""
